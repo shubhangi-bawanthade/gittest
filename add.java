e01d4e127e25669597a851a7c6e53881001d9245
shubhangi bawanthade <shubhangi.bawanthade@gmail.com>
2021-08-25 15:08:51 +0530

    change add file

--- add.java
+++ add.java
@@ -3,6 +3,6 @@ public class add
     public static void main(String[] args) {
         int a=3;
         int b=4;
-        System.out.println("add="+(a+b));
+        System.out.println("addition ="+(a+b));
     }
 }
